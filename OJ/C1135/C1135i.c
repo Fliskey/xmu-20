6047825e9e1c7f9bef69d58d2fc1df5557037ed8
Fliskey <Fliskey@qq.com>
2020-11-03 19:32:24 +0800

    C1135i ac, tought by Xusheng Wang

--- OJ/C1135/C1135i.c
+++ OJ/C1135/C1135i.c
@@ -3,25 +3,35 @@
 int main(){
 	int n;
 	scanf("%d",&n);
+	int if_printed = 0;
 	for(int i=n;i>=0;i--){
 		int xs;
 		scanf("%d",&xs);
 		if(xs != 0){
-			if(i == n){
+			if(!if_printed){
+				if_printed = 1;
 				if(i == 1){
 					if(xs != 1)
 						printf("%dx",xs);
+					else if(xs == -1)
+						printf("-x");
 					else
 						printf("x");
 				}
+				else if(i == 0){
+					printf("%d",xs);
+				}
 				else{
-					if(xs != 1)
+					if(xs == -1)
+						printf("-x^%d",i);
+					else if(xs != 1)
 						printf("%dx^%d",xs,i);
 					else
 						printf("x^%d",i);
 				}
 			}
 			else{
+				if_printed = 1;
 				if(i == 1){
 					if(xs>0){
 						if(xs != 1)
@@ -60,6 +70,9 @@ int main(){
 				}
 			}
 		}
+	}
+	if(!if_printed){
+		printf("0");
 	}
 	return 0;
 }
