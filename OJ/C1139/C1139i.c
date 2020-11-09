c07ab7a3df7abff9ecc47f2e4c12cf7ae9e933f4
Fliskey <Fliskey@qq.com>
2020-11-09 22:11:01 +0800

    C1139 update

--- OJ/C1139/C1139i.c
+++ OJ/C1139/C1139i.c
@@ -8,20 +8,54 @@ typedef struct Node{
 }Node,*Linklist;
 
 int main(){
-    int n;
-    while(scanf("%d",&n) != EOF){
+    int n,m;
+    int is_a = 1;
+    while(scanf("%d %d",&n,&m) != EOF){
+        if(!is_a){
+            printf("\n");
+        }
         Linklist Head;
+        Head = (Linklist) malloc(sizeof(Node));
         Head->data = 0;
         Head->last = Head;
         Head->next = Head;
         for(int i=1;i<=n;i++){
-            Linklist p;
+            Linklist p = (Linklist) malloc(sizeof(Node));
             p->data = i;
 
             Head->last->next = p;
             p->last = Head->last;
             Head->last = p;
             p->next = Head;
+        }
+        int is_first = 1;
+        Linklist p = Head->next;
+        while(n!=0){
+            for(int i=2;i<=m;i++){
+                p = p->next;
+                if(p->data == 0){
+                	p = p->next;
+				}
+            }
+            if(is_first){
+                printf("%d",p->data);
+                is_first = 0;
+            }
+            else{
+                printf(" %d",p->data);
+            }
+            p->last->next = p->next;
+            p->next->last = p->last;
+            Linklist del = p;
+            p = p->next;
+            if(p->data == 0){
+            	p = p->next;
+			}
+            free(del);
+            n--;
+        }
+        if(is_a){
+            is_a = 0;
         }
     }
     return 0;
