7d34e6cd2cfd7b79908a1afa05e3d2c2dbf75458
Fliskey <Fliskey@qq.com>
2020-11-30 11:07:20 +0800

    remove function declear in main

--- 2048/enter.c
+++ 2048/enter.c
@@ -17,22 +17,6 @@ const int TARGET=64;
 int board[6][6] = {0};
 int maxvalue = 0;
 int goal = 0;
-
-//function declear
-int InitBoard();
-int SetRandom();
-
-int Merge(int);
-int Move(int);
-
-int ClearBoard();
-void ClearS();
-int PrintBoard();
-
-int GetInput();
-int FailJudge();
-int Failed();
-int Succeed();
  
 
 //---------------- MAIN FUNCTION ----------------
